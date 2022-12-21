9d3887788267f3604c1f2e70121e39ce81c9b468
Vincius Vequeti <viniciusvequeti@gmail.com>
2022-12-21 08:42:17 -0300

    Improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,7 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
+//import boardgame.Position;
 import chess.piece.Bishop;
 import chess.piece.King;
 import chess.piece.Knight;
@@ -39,5 +39,13 @@ public class ChessMatch {
 		placeNewPiece('f', 8, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 8, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
+		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
+		placeNewPiece('b', 1, new Knight(board, Color.BLACK));
+		placeNewPiece('c', 1, new Bishop(board, Color.BLACK));
+		placeNewPiece('d', 1, new Queen(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.BLACK));
+		placeNewPiece('f', 1, new Bishop(board, Color.BLACK));
+		placeNewPiece('g', 1, new Knight(board, Color.BLACK));
+		placeNewPiece('h', 1, new Rook(board, Color.BLACK));
 	}
 }
