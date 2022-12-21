b927c276fc7dc57788ff72b8327f15b603d9585b
Vincius Vequeti <viniciusvequeti@gmail.com>
2022-12-21 14:23:52 -0300

    Start of implementation of possible moves and implemented Rook moves

--- src/chess/piece/Knight.java
+++ src/chess/piece/Knight.java
@@ -14,4 +14,10 @@ public class Knight extends ChessPiece {
 	public String toString() {
 		return "N";
 	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
