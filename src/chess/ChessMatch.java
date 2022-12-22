90ea7802f0feee7b7a7dfd56408a2cb3729bd154
Vincius Vequeti <viniciusvequeti@gmail.com>
2022-12-22 10:00:49 -0300

    Corrected placements and implemented turn system

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,11 +11,23 @@ import chess.piece.Queen;
 import chess.piece.Rook;
 
 public class ChessMatch {
+	
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -40,6 +52,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -54,6 +67,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
 		if (!board.piece(position).isThereAnyPossibleMove()){
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
@@ -65,6 +81,11 @@ public class ChessMatch {
 		}
 	}
 	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+	
 	private void placeNewPiece (char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
@@ -73,16 +94,16 @@ public class ChessMatch {
 		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
 		placeNewPiece('b', 8, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 8, new Bishop(board, Color.WHITE));
-		placeNewPiece('d', 8, new Queen(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.WHITE));
+		placeNewPiece('d', 8, new King(board, Color.WHITE));
+		placeNewPiece('e', 8, new Queen(board, Color.WHITE));
 		placeNewPiece('f', 8, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 8, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
 		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
 		placeNewPiece('b', 1, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 1, new Bishop(board, Color.BLACK));
-		placeNewPiece('d', 1, new Queen(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.BLACK));
+		placeNewPiece('d', 1, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new Queen(board, Color.BLACK));
 		placeNewPiece('f', 1, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 1, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 1, new Rook(board, Color.BLACK));
